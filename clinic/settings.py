6fc7172459b487c03f7f3e5cac831d369f4c7958
rodgersxy <rodgersny99@gmail.com>
2023-10-23 11:08:40 +0300

    update

--- clinic/settings.py
+++ clinic/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-s8ld^13u%0mzg+flj(x(ba03aei931p!t!lg=k%z-xe94)iso_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
